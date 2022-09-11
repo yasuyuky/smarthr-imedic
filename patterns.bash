d6dc2e34da49b1737abdee4f1e2847f272914162
yasuyuky <yasuyuki.ymd@gmail.com>
2022-09-11 21:53:56 +0900

    Update the utility script

--- patterns.bash
+++ patterns.bash
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash -e
 
+EXEC="docker run --rm -e SMARTHR_TENANT=$SMARTHR_TENANT -e SMARTHR_TOKEN=$SMARTHR_TOKEN ghcr.io/yasuyuky/smarthr-imedic"
+
 for f in tsv csv; do
     mkdir -p output/$f
-    python create_dict.py $f last last >output/$f/${SMARTHR_TENANT}$(date +"%Y%m%d").last-last.txt
-    python create_dict.py $f full full >output/$f/${SMARTHR_TENANT}$(date +"%Y%m%d").full-full.txt
-    python create_dict.py $f last full >output/$f/${SMARTHR_TENANT}$(date +"%Y%m%d").last-full.txt
-    python create_dict.py $f last email >output/$f/${SMARTHR_TENANT}$(date +"%Y%m%d").last-email.txt
+    $EXEC $f last last >output/$f/${SMARTHR_TENANT}$(date +"%Y%m%d").last-last.txt
+    $EXEC $f full full >output/$f/${SMARTHR_TENANT}$(date +"%Y%m%d").full-full.txt
+    $EXEC $f last full >output/$f/${SMARTHR_TENANT}$(date +"%Y%m%d").last-full.txt
+    $EXEC $f last email >output/$f/${SMARTHR_TENANT}$(date +"%Y%m%d").last-email.txt
 done
