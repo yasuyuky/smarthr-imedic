e75df2613951af7027d76426d166dd45274f152b
yasuyuky <yasuyuki.ymd@gmail.com>
2026-03-21 15:26:09 +0900

    Show created output files after running patterns script

--- patterns.bash
+++ patterns.bash
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash -e
 
 EXEC="docker run --rm -e SMARTHR_TENANT=$SMARTHR_TENANT -e SMARTHR_TOKEN=$SMARTHR_TOKEN ghcr.io/yasuyuky/smarthr-imedic"
+OUTPUTS=()
 
 for f in tsv csv; do
     mkdir -p output/$f
-    $EXEC $f last last >output/$f/${SMARTHR_TENANT}$(date +"%Y%m%d").last-last.txt
-    $EXEC $f full full >output/$f/${SMARTHR_TENANT}$(date +"%Y%m%d").full-full.txt
-    $EXEC $f last full >output/$f/${SMARTHR_TENANT}$(date +"%Y%m%d").last-full.txt
-    $EXEC $f last email >output/$f/${SMARTHR_TENANT}$(date +"%Y%m%d").last-email.txt
+    for pattern in "last last" "full full" "last full" "last email"; do
+        set -- $pattern
+        output=output/$f/${SMARTHR_TENANT}$(date +"%Y%m%d").$1-$2.txt
+        $EXEC $f "$1" "$2" >"$output"
+        OUTPUTS+=("$output")
+    done
 done
+
+printf 'Created files:\n'
+printf '  %s\n' "${OUTPUTS[@]}"
